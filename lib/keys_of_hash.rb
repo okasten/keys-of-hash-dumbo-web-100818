c2da9b8e7bba8f90ec8f987e3c55d0f8a8ace6ec
Olivia Kasten <odkasten@gmail.com>
2018-09-26 16:04:17 +0000

    automatically backed up by learn

--- lib/keys_of_hash.rb
+++ lib/keys_of_hash.rb
@@ -3,9 +3,11 @@ require "pry"
 class Hash
   def keys_of(*arguments)
     arr = []
-    if arguments == :animals[]
-    :animals.each do |arg|
-      if arguments == :animals[]
-    end 
+    self.each do |k, v|
+      arguments.each do |arg|
+        if arg == v 
+          arr << k
+        end 
+      end 
   end
 end
